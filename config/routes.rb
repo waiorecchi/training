1439f2407612be828f6300dd93f3407b6c2eb412
waiorecchi <102579111+waiorecchi@users.noreply.github.com>
2025-07-09 05:46:14 +0000

    s

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+
+  get 'homes/about' => 'homes#about', as: 'about'
+end
+
